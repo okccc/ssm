4228d6789a083d1e84ff625b3e7068d5b917b90d
okccc <1573976179@qq.com>
2024-04-06 09:45:54 +0800

    mybatis开发步骤

---
+++ ssm-mybatis/src/main/java/com/okccc/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.okccc.mapper;
+
+/**
+ * @Author: okccc
+ * @Date: 2023/10/18 16:49:53
+ * @Desc:
+ *
+ * mybatis开发步骤
+ * 1.创建maven工程导入依赖
+ * 2.创建mybatis-config.xml,引入属性文件、全局配置、设置类型别名、分页插件、配置数据库连接、引入映射文件
+ * 3.创建Mapper接口和对应的映射文件执行crud操作
+ */
+public interface UserMapper {
+}
