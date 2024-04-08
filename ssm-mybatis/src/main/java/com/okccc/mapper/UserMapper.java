d04b6b8e078d783911107b332cc8a1e5cf24ccff
okccc <1573976179@qq.com>
2024-04-08 10:13:28 +0800

    idea设置mybatis核心配置文件和映射文件模板

--- ssm-mybatis/src/main/java/com/okccc/mapper/UserMapper.java
+++ ssm-mybatis/src/main/java/com/okccc/mapper/UserMapper.java
@@ -5,6 +5,9 @@ package com.okccc.mapper;
  * @Date: 2023/10/18 16:49:53
  * @Desc:
  *
+ * idea设置mybatis核心配置文件和映射文件模板
+ * Intellij IDEA - Settings - Editor - File and Code Templates - Files - mybatis-config & mybatis-mapper
+ *
  * mybatis开发步骤
  * 1.创建maven工程导入依赖
  * 2.创建mybatis-config.xml,引入属性文件、全局配置、设置类型别名、分页插件、配置数据库连接、引入映射文件
