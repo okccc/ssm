4d80ea7f99e2639c35270f66ce86744691577afc
okccc <1573976179@qq.com>
2024-04-09 10:25:30 +0800

    mybatis获取参数值的两种方式

--- ssm-mybatis/src/main/java/com/okccc/mapper/UserMapper.java
+++ ssm-mybatis/src/main/java/com/okccc/mapper/UserMapper.java
@@ -12,6 +12,11 @@ package com.okccc.mapper;
  * 1.创建maven工程导入依赖
  * 2.创建mybatis-config.xml,引入属性文件、全局配置、设置类型别名、分页插件、配置数据库连接、引入映射文件
  * 3.创建Mapper接口和对应的映射文件执行crud操作
+ *
+ * mybatis获取参数值的两种方式
+ * 1.#{}占位符赋值,where id = #{id} -> where id = ? (推荐)
+ * 2.${}字符串拼接,where username = '${username}' -> where username = 'grubby' 可能存在sql注入问题
+ * 能用#{}实现就不用${},特殊场景才会使用字符串拼接,比如动态的不是value值而是表名/列名/关键字
  */
 public interface UserMapper {
 }
