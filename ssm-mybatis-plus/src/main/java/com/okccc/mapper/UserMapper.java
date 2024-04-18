a8df6c245fd7af467b76d257df5bd79ed5efedc9
okccc <1573976179@qq.com>
2024-04-18 10:06:22 +0800

    逆向工程插件MybatisX

--- ssm-mybatis-plus/src/main/java/com/okccc/mapper/UserMapper.java
+++ ssm-mybatis-plus/src/main/java/com/okccc/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.okccc.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.okccc.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Author: okccc
@@ -12,7 +11,11 @@ import org.apache.ibatis.annotations.Mapper;
  * ORM(Object-Relational Mapping)：将java对象和数据库表进行映射,用面向对象的思维通过调用方法操作数据库
  * 半自动ORM框架：xml指定实体类和数据库表的映射,手动编写sql可以优化,手动将查询结果转换成实体类(MyBatis)
  * 全自动ORM框架：自动进行实体类和数据库表的映射,自动组装sql无法优化,自动将查询结果转换成实体类(Hibernate、MyBatis-Plus)
+ *
+ * mybatis逆向工程(半自动 -> 全自动)
+ * 单表查询无需优化,要是能像Hibernate那样全自动生成就好了,于是MyBatis的逆向工程出现了,但是多表查询依然需要手动编写
+ * MyBatisX插件可以自动生成持久层代码,包括MyBatis的Pojo类、Mapper.java接口、Mapper.xml映射文件,大大简化开发流程
+ * 步骤：idea右侧Database插件 - 选中数据库表 - 右键MyBatisX-Generator - base package、relative package - mybatis-plus3、Lombok、Model、mybatis-plus3
  */
-@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
