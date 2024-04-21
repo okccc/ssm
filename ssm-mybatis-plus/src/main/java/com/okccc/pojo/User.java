7d4b0b9561e9c9d30a29f18e6b5bbf8ae66040a2
okccc <1573976179@qq.com>
2024-04-21 10:31:46 +0800

    数据拆分主键重复问题

--- ssm-mybatis-plus/src/main/java/com/okccc/pojo/User.java
+++ ssm-mybatis-plus/src/main/java/com/okccc/pojo/User.java
@@ -18,9 +18,11 @@ import lombok.NoArgsConstructor;
  *
  * 2.@TableId
  * 作用于主键列,mysql数据库的主键策略默认是auto_increment,如果要使用其它策略可以添加该注解修改
- *
- * 3.@TableField
- * 作用于非主键列,当实体类属性名和数据库列名不一致时添加该注解指定
+ * 数据库表太大要做数据拆分
+ * 垂直拆分是列拆分,经常查询的列就是热数据,冷热数据是一条数据拆开的,所以是同一个主键,新增数据也就不存在主键重复的问题
+ * 水平拆分是行拆分,比如上亿条数据必须分库分表,如果主键策略是自增长,拆分的多个表新增数据会出现主键重复的问题,此时可以用UUID或雪花算法
+ * UUID：随机生成一个不重复的字符串,算法复杂且字符串太长效率较低
+ * 雪花算法：随机生成一个不重复的数字,实体类用Long或String,数据库用bigint或varchar,解决分布式系统中生成全局唯一id的需求
  *
  * CREATE TABLE `t_user` (
  *   `id` bigint(20) PRIMARY KEY NOT NULL,
@@ -49,6 +51,8 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @TableId(type = IdType.AUTO)
+//    @TableId(type = IdType.ASSIGN_UUID)
+//    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
     @TableField(value = "username", exist = true)
